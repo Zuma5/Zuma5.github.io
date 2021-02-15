91414906f0999b4b4e2c366f235e5d7cd380d42e
Zuma5 <Ozi.Zimina@gmail.com>
2021-02-15 06:10:20 +0200

    new

--- digital/js/digital.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".click").click(function() {
-		$(".hide").fadeToggle(600);
-		$(".hide").css("background", "rgba(0,0,0, .3)");
-		$(".hide").css("color", "#ececec");
-	});
-});
